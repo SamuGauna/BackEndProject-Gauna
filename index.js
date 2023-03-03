487a0c618b4dee73052860599c1fa2c703ded2f0
samu.el <samuelgauna78@gmail.com>
2023-03-02 23:17:18 -0300

    correccion

--- index.js
+++ index.js
@@ -5,18 +5,32 @@ class ProductManager {
         this.products = []
     }
     static id = 0 
-
+    
     getProducts(){
         console.log(this.products);
         return this.products
         
     }
     addProduct(title, description, price, thumbnail, code, stock){
-        this.products.push(title, description, price, thumbnail, code, stock, ProductManager.id++)       
+        if ( title && description && price && thumbnail && code && stock) {
+            const Producto = {
+                title: title,
+                description: description,
+                price: price,
+                thumbnail: thumbnail,
+                code: code,
+                stock: stock,
+                id: ProductManager.id++
+            }
+            return this.products.push(Producto) 
+        } else {
+            console.error(`Al producto le faltan datos`)
+        }
+            
     }
-    getProductById(){
-        if (this.products.filter(item => item.id <= 3)) {
-            console.log(...this.products)
+    getProductById(id){
+        if (this.products.find(item => item.id == id)) {
+            console.log('El producto que buscas si esta en tu inventario')
         } else {
             console.log("Not found")
         }
@@ -25,11 +39,15 @@ class ProductManager {
 }
 const product = new ProductManager()
 product.addProduct('Escoba', 'Semi nueva', 77, 'https://http2.mlstatic.com/D_NQ_NP_2X_780827-MLA52027354635_102022-F.webp', "1a", 5);
-product.addProduct('Mopa', 'Nueva', 88, 'https://http2.mlstatic.com/D_NQ_NP_2X_724551-MLA32041668736_092019-F.webp', "2b", 7 );
-product.addProduct('Secador de piso', 'Un solo uso', 99, 'https://http2.mlstatic.com/D_NQ_NP_2X_775191-MLA53364171913_012023-F.webp', "3c", 2);
 
 product.getProducts();
 
-product.getProductById()
+  
 
+ product.addProduct('Mopa', 'Nueva', 88, 'https://http2.mlstatic.com/D_NQ_NP_2X_724551-MLA32041668736_092019-F.webp', "2b",  );
 
+ product.addProduct('Secador de piso', 'Un solo uso', 99, 'https://http2.mlstatic.com/D_NQ_NP_2X_775191-MLA53364171913_012023-F.webp', "3c", 2);
+
+ product.getProductById(1)
+
+ product.getProducts();
